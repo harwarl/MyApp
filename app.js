dcd89cc79320ca2e7abc6db4634b89566551dda2
Oduwale Awwal <harwarl87@gmail.com>
2022-10-28 22:45:21 +0100

    WHat did I do?

--- app.js
+++ app.js
@@ -1,36 +1,41 @@
-// const express = require("express");
-// const bodyparser = require("body-parser");
-// PORT = 3000;
-
-// const app = express();
-
-// app.use(bodyparser.urlencoded({extended: false}))
-
-// app.use('/', (req, res, next)=>{
-//     res.send("<h1>Hello World</h1>");
-// })
-
-// app.listen(PORT, err=>{
-//     if(err){
-//         console.log(err)
-//     }
-//     console.log("Server is Up and Running on Port ", PORT);
-// })
-
+const express = require("express");
+const bodyparser = require("body-parser");
 const PORT = process.env.PORT || 3000;
 
-const http = require("http");
-const requestListener = (req, res)=>{
-    const responseData = {
-        slackUsername: "ErenJs",
-        backend: true,
-        age: 23,
-        bio: "Hello, I am a beginner. I don't know much and I hope to learn through HNG"
-    }
-
-    const jsonContent = JSON.stringify(responseData);
-    res.write(jsonContent);
-    res.end();
+const app = express();
+const responseData = {
+    slackUsername: "ErenJs",
+    backend: true,
+    age: 23,
+    bio: "Hello, I am a beginner. I don't know much and I hope to learn through HNG"
 }
-const httpServer = http.createServer(requestListener);
-httpServer.listen(PORT);
+
+const jsonContent = JSON.stringify(responseData);
+
+app.use(bodyparser.urlencoded({extended: false}))
+app.use('/bio', (req, res)=>{
+    res.send(JSON.parse(jsonContent).bio);
+})
+
+app.use('/age', (req, res)=>{
+    res.send(JSON.parse(jsonContent).age.toString());
+})
+
+app.use('/backend', (req, res)=>{
+    res.send(JSON.parse(jsonContent).backend);
+})
+
+app.use('/slackUsername', (req, res)=>{
+    res.send(JSON.parse(jsonContent).slackUsername);
+})
+
+app.use('/', (req, res, next)=>{
+    res.send(jsonContent);
+})
+
+app.listen(PORT, err=>{
+    if(err){
+        console.log(err)
+    }
+    console.log("Server is Up and Running on Port ", PORT);
+})
